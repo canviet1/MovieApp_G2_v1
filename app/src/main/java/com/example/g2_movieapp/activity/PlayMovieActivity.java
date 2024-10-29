cc91de95ebd702ff76cfda0adb03ea4cafeae899
hieuxop <hieuxopbi@gmail.com>
2024-10-29 23:37:34 +0700

    PlayMovieActivity

--- app/src/main/java/com/example/g2_movieapp/activity/PlayMovieActivity.java
+++ app/src/main/java/com/example/g2_movieapp/activity/PlayMovieActivity.java
@@ -23,17 +23,226 @@ import androidx.appcompat.app.AppCompatActivity;
 import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
 
+import com.android.volley.Request;
 import com.example.g2_movieapp.R;
+import com.example.g2_movieapp.adapter.MovieListAdapter;
+import com.example.g2_movieapp.domain.Movie;
+import com.example.g2_movieapp.domain.MovieData;
+import com.example.g2_movieapp.domain.Response;
+import com.example.g2_movieapp.domain.user.TrailerResponse;
+import com.example.g2_movieapp.helper.Constant;
+import com.example.g2_movieapp.helper.GsonRequest;
+import com.example.g2_movieapp.helper.RequestQueueSingleton;
+import com.example.g2_movieapp.interfaces.OnHeartClickListener;
+
+import org.json.JSONException;
+import org.json.JSONObject;
 
 
-public class PlayMovieActivity extends AppCompatActivity {
+public class PlayMovieActivity extends AppCompatActivity implements OnHeartClickListener {
+    private WebView webView;
+    private RecyclerView.Adapter adapter;
+    private ImageView backImg, ivAddfavorite;
+    private Context context;
+    private int idMovie;
+    private ProgressBar loading;
+    private RecyclerView recyclerView;
+    private FrameLayout mFullscreenContainer;
+    private View mCustomView;
+    private WebChromeClient.CustomViewCallback mCustomViewCallback;
+    private int mOriginalOrientation;
+    private int mOriginalSystemUiVisibility;
+    private TextView movieName;
+    private TextView description;
 
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_youtube_player);
+        context = this;
+        webView = findViewById(R.id.playerView);
+        idMovie = getIntent().getIntExtra("id", 0);
+        initView();
+        sendRequest();
+        sendRequest1();
+        sendRequest2();
 
+        ivAddfavorite = findViewById(R.id.addfavorite);
+        ivAddfavorite.setOnClickListener(v -> onHeartClick(idMovie, true));
+
+        backImg = findViewById(R.id.backImg);
+        backImg.setOnClickListener(v -> finish());
     }
 
+    private void initView() {
+        recyclerView = findViewById(R.id.rvRecommendationMovies);
+        recyclerView.setLayoutManager(new LinearLayoutManager(this, LinearLayoutManager.HORIZONTAL, false));
+        movieName = findViewById(R.id.movieName);
+        description = findViewById(R.id.description);
+        loading = findViewById(R.id.loadingRecommendation);
+    }
+    private void sendRequest2() {
+        loading.setVisibility(View.VISIBLE);
 
+        String url = Constant.API.GET_MOVIE + idMovie+"?api_key=5aa5ed76193d3d2a01f9f679885ec8d3&language=en-US&page=1";
+        GsonRequest<Movie> gsonRequest = new GsonRequest<>(this, Request.Method.GET,url,Movie.class,null,
+                response -> {
+                    loading.setVisibility(View.GONE);
+                    Movie movie = response;
+
+                    movieName.setText(movie.getTitle());
+                    description.setText(movie.getOverview());
+                },
+                error -> {
+                    loading.setVisibility(View.GONE);
+                    Log.e("VOLLEY", "Error: " + error.getMessage());
+                    Toast.makeText(context, "Failed to load movie details", Toast.LENGTH_SHORT).show();
+                }
+        );
+
+        RequestQueueSingleton.getInstance(this).addToRequestQueue(gsonRequest);
+    }
+    private void sendRequest1() {
+        loading.setVisibility(View.VISIBLE);
+        SharedPreferences prefs = getSharedPreferences(Constant.App.PREFS_SESSION, Context.MODE_PRIVATE);
+        String Se_id = prefs.getString("session_id","");
+        String url = Constant.API.GET_RECOMMENDATIONS.replace("{id}", String.valueOf(idMovie))+Se_id;
+
+        GsonRequest<MovieData> gsonRequest = new GsonRequest(this, Request.Method.GET, url, MovieData.class,
+                response -> {
+                    Log.i("request api: ", "get api success");
+                    loading.setVisibility(View.GONE);
+                    adapter = new MovieListAdapter((MovieData) response, R.layout.viewholder_film, null);
+                    recyclerView.setAdapter(adapter);
+                }, error -> {
+            Log.i("request api error: ", error.getMessage());
+            loading.setVisibility(View.GONE);
+        });
+
+        RequestQueueSingleton.getInstance(this).addToRequestQueue(gsonRequest);
+    }
+
+    private void sendRequest() {
+        String url = Constant.API.GET_MOVIE_VIDEOS(idMovie);
+        GsonRequest<TrailerResponse> gsonRequest = new GsonRequest<>(
+                this,
+                Request.Method.GET,
+                url,
+                TrailerResponse.class,
+                null,
+                response -> {
+                    TrailerResponse.Trailer selectTrailer = response.getResults().get(0);
+                    playMovie(selectTrailer.getKey());
+                },
+                error -> {
+                    Log.e("VOLLEY", "Error: " + error.getMessage());
+                    Toast.makeText(this, "Failed to load movie video", Toast.LENGTH_SHORT).show();
+                }
+        );
+        RequestQueueSingleton.getInstance(this).addToRequestQueue(gsonRequest);
+    }
+
+    private void playMovie(String movieKey) {
+        WebSettings webSettings = webView.getSettings();
+        webSettings.setJavaScriptEnabled(true);
+        webSettings.setLoadWithOverviewMode(true);
+        webSettings.setUseWideViewPort(true);
+
+        webView.setWebViewClient(new WebViewClient());
+        webView.setWebChromeClient(new WebChromeClient() {
+            @Override
+            public void onShowCustomView(View view, CustomViewCallback callback) {
+                if (mCustomView != null) {
+                    callback.onCustomViewHidden();
+                    return;
+                }
+                mCustomView = view;
+                mOriginalSystemUiVisibility = getWindow().getDecorView().getSystemUiVisibility();
+                mOriginalOrientation = getRequestedOrientation();
+                mCustomViewCallback = callback;
+
+                getWindow().getDecorView().setSystemUiVisibility(
+                        View.SYSTEM_UI_FLAG_FULLSCREEN
+                                | View.SYSTEM_UI_FLAG_HIDE_NAVIGATION
+                                | View.SYSTEM_UI_FLAG_IMMERSIVE_STICKY);
+
+                FrameLayout decor = (FrameLayout) getWindow().getDecorView();
+                mFullscreenContainer = new FrameLayout(PlayMovieActivity.this);
+                mFullscreenContainer.setBackgroundColor(Color.BLACK);
+                mFullscreenContainer.addView(mCustomView, new FrameLayout.LayoutParams(
+                        ViewGroup.LayoutParams.MATCH_PARENT,
+                        ViewGroup.LayoutParams.MATCH_PARENT));
+                decor.addView(mFullscreenContainer, new FrameLayout.LayoutParams(
+                        ViewGroup.LayoutParams.MATCH_PARENT,
+                        ViewGroup.LayoutParams.MATCH_PARENT));
+
+
+            }
+
+
+            @Override
+            public void onHideCustomView() {
+                if (mCustomView == null)
+                    return;
+
+                FrameLayout decor = (FrameLayout) getWindow().getDecorView();
+                decor.removeView(mFullscreenContainer);
+                mFullscreenContainer = null;
+                mCustomView = null;
+                getWindow().getDecorView().setSystemUiVisibility(mOriginalSystemUiVisibility);
+                setRequestedOrientation(mOriginalOrientation);
+                mCustomViewCallback.onCustomViewHidden();
+                mCustomViewCallback = null;
+
+                setRequestedOrientation(ActivityInfo.SCREEN_ORIENTATION_PORTRAIT);
+            }
+        });
+
+        String html = getHtml(movieKey);
+        webView.loadData(html, "text/html", "utf-8");
+    }
+
+    private String getHtml(String videoId) {
+        return "<html><body>" +
+                "<iframe width=\"100%\" height=\"100%\" src=\"https://www.youtube.com/embed/" + videoId + "\" frameborder=\"0\" allowfullscreen></iframe>" +
+                "</body></html>";
+    }
+
+    @Override
+    public void onHeartClick(int idMovie, boolean fav) {
+        JSONObject jsonBody = new JSONObject();
+        try {
+            jsonBody.put("media_id", idMovie);
+            jsonBody.put("media_type", "movie");
+            jsonBody.put("favorite", fav);
+        } catch (JSONException e) {
+            throw new RuntimeException(e);
+        }
+        final String requestBody = jsonBody.toString();
+        GsonRequest<Response> gsonRequest = new GsonRequest<>(
+                this,
+                Request.Method.POST,
+                Constant.API.postFavoriteMovieApi(this),
+                Response.class,
+                requestBody,
+                response -> {
+                    Log.i("VOLLEY", "Success");
+                    if (fav) {
+                        Toast.makeText(context, "Added to favorites", Toast.LENGTH_SHORT).show();
+                    } else {
+                        Toast.makeText(context, "Removed from favorites", Toast.LENGTH_SHORT).show();
+                    }
+                },
+                error -> {
+                    Log.i("VOLLEY", "Failure");
+                    if (fav) {
+                        Toast.makeText(context, "Failed to add to favorites", Toast.LENGTH_SHORT).show();
+                    } else {
+                        Toast.makeText(context, "Failed to remove from favorites", Toast.LENGTH_SHORT).show();
+                    }
+                }
+        );
+
+        RequestQueueSingleton.getInstance(context).addToRequestQueue(gsonRequest);
+    }
 }
---
+++ app/src/main/java/com/example/g2_movieapp/adapter/MovieListAdapter.java
@@ -0,0 +1,202 @@
+package com.example.g2_movieapp.adapter;
+
+import android.content.Context;
+import android.content.Intent;
+import android.content.SharedPreferences;
+import android.util.Log;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ImageView;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import androidx.annotation.ColorInt;
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import androidx.core.content.ContextCompat;
+import androidx.recyclerview.widget.RecyclerView;
+
+import com.android.volley.Request;
+import com.bumptech.glide.Glide;
+import com.example.g2_movieapp.R;
+import com.example.g2_movieapp.activity.DetailActivity;
+import com.example.g2_movieapp.domain.Movie;
+import com.example.g2_movieapp.domain.MovieData;
+import com.example.g2_movieapp.domain.Response;
+import com.example.g2_movieapp.helper.Constant;
+import com.example.g2_movieapp.helper.GsonRequest;
+import com.example.g2_movieapp.helper.RequestQueueSingleton;
+import com.example.g2_movieapp.helper.Util;
+import com.example.g2_movieapp.interfaces.OnHeartClickListener;
+import com.example.g2_movieapp.interfaces.TaskCallback;
+import com.shashank.sony.fancytoastlib.FancyToast;
+
+import org.json.JSONException;
+import org.json.JSONObject;
+
+
+public class MovieListAdapter extends RecyclerView.Adapter<MovieListAdapter.ViewHolder> implements OnHeartClickListener {
+
+    private MovieData items;
+    private Context context;
+    private int layoutResourceId;
+    private String favoriteMovieString;
+    private TaskCallback taskCallback;
+
+    public MovieListAdapter(MovieData items, int layoutResourceId, @Nullable TaskCallback taskCallback) {
+        this.items = items;
+        this.layoutResourceId = layoutResourceId;
+        this.taskCallback = taskCallback;
+    }
+    public void updateData(MovieData newItems) {
+        this.items = newItems;
+        notifyDataSetChanged();
+    }
+
+    public MovieListAdapter(MovieData items) {
+        this.items = items;
+    }
+
+    @NonNull
+    @Override
+    public ViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
+        View inflate = LayoutInflater.from(parent.getContext()).inflate(layoutResourceId, parent, false);
+        context = parent.getContext();
+        return new ViewHolder(inflate);
+    }
+
+    @Override
+    public void onBindViewHolder(@NonNull ViewHolder holder, int position) {
+        Movie movie = items.getResults().get(position);
+        String movieId = movie.getId().toString();
+
+        holder.idTxt.setText(movieId);
+        holder.titleTxt.setText(movie.getTitle());
+        holder.scoreTxt.setText(movie.getVoteAverage() == null ? "" : movie.getVoteAverage().toString());
+
+        Glide.with(holder.itemView.getContext())
+                .load(Constant.API.IMAGE + movie.getPosterPath())
+                .into(holder.pic);
+
+        holder.itemView.setOnClickListener(v -> {
+            Intent intent = new Intent(holder.itemView.getContext(), DetailActivity.class);
+            intent.putExtra("id", movie.getId());
+            holder.itemView.getContext().startActivity(intent);
+        });
+
+        SharedPreferences prefs = context.getSharedPreferences(Constant.App.PREFS_SESSION, Context.MODE_PRIVATE);
+        favoriteMovieString = prefs.getString("favorite_movie_list", "");
+
+        boolean isFav = false;
+        String[] favoriteMovieIds = favoriteMovieString.split(",");
+        for (String id :
+                favoriteMovieIds) {
+            if (id.equals(movieId)) {
+                isFav = true;
+            }
+        }
+
+        holder.ivAddfavorite.setOnClickListener(v -> {
+            TextView favoriteStatus = holder.favoriteStatus;
+            boolean fav = favoriteStatus.getText().toString().equals("true");
+            favoriteStatus.setText(fav ? "false" : "true");
+            onHeartClick(movie.getId(), !fav);
+        });
+
+        if (isFav) {
+            // Get the color from resources
+            @ColorInt int color = ContextCompat.getColor(context, R.color.favorite_color);
+            holder.ivAddfavorite.setColorFilter(color);
+            holder.favoriteStatus.setText("true");
+        } else {
+            @ColorInt int color = ContextCompat.getColor(context, R.color.white);
+            holder.ivAddfavorite.setColorFilter(color);
+            holder.favoriteStatus.setText("false");
+        }
+    }
+
+    @Override
+    public int getItemCount() {
+        return items.getResults().size();
+    }
+
+//    private void updateFavoriteStatus(boolean isFav) {
+//
+//    }
+
+    @Override
+    public void onHeartClick(int movieId, boolean fav) {
+        JSONObject jsonBody = new JSONObject();
+        try {
+            jsonBody.put("media_id", movieId);
+            jsonBody.put("media_type", "movie");
+            jsonBody.put("favorite", fav);
+        } catch (JSONException e) {
+            throw new RuntimeException(e);
+        }
+        final String requestBody = jsonBody.toString();
+
+        GsonRequest<Response> gsonRequest = new GsonRequest<>(
+                context,
+                Request.Method.POST,
+                Constant.API.postFavoriteMovieApi(context),
+                Response.class,
+                requestBody,
+                response -> {
+                    Log.i("VOLLEY", "thành công");
+                    if (fav) {
+                        FancyToast.makeText(context, "Added to favorites", FancyToast.LENGTH_LONG, FancyToast.SUCCESS, true).show();
+                    } else {
+                        FancyToast.makeText(context, "Removed from favorites", FancyToast.LENGTH_LONG, FancyToast.INFO, true).show();
+                    }
+
+                    if (taskCallback != null) {
+                        Util.saveFavoriteList(context, taskCallback);
+                    }
+
+                }, error -> {
+            Log.i("VOLLEY", "thất bại");
+            if (fav) {
+                Toast.makeText(context, "Failed to add to favorites", Toast.LENGTH_SHORT).show();
+            } else {
+                Toast.makeText(context, "Failed to remove from favorites", Toast.LENGTH_SHORT).show();
+            }
+        });
+
+        RequestQueueSingleton.getInstance(context).addToRequestQueue(gsonRequest);
+    }
+
+
+    public class ViewHolder extends RecyclerView.ViewHolder {
+        TextView titleTxt, scoreTxt, idTxt, favoriteStatus;
+        ImageView pic;
+        ImageView ivAddfavorite, ivRemovefavorite;
+//        OnHeartClickListener onHeartClickListener;
+
+        public ViewHolder(@NonNull View itemView) {
+            super(itemView);
+            titleTxt = itemView.findViewById(R.id.titleTxt);
+            scoreTxt = itemView.findViewById(R.id.scoreTxt);
+            pic = itemView.findViewById(R.id.pic);
+            idTxt = itemView.findViewById(R.id.idTxt);
+            ivAddfavorite = itemView.findViewById(R.id.addfavorite);
+            ivRemovefavorite = itemView.findViewById(R.id.removefavorite);
+            favoriteStatus = itemView.findViewById(R.id.favoriteStatus);
+
+//            this.onHeartClickListener = onHeartClickListener;
+//
+//            ivAddfavorite.setOnClickListener(v -> {
+//                onHeartClickListener.onHeartClick(getAdapterPosition(), true);
+//            });
+
+//            if (ivRemovefavorite != null) {
+//                ivRemovefavorite.setOnClickListener(v -> {
+//                    onHeartClickListener.onHeartClick(getAdapterPosition(), false);
+//                });
+//            }
+        }
+    }
+}
+
+
