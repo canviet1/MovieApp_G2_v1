6d9a880b972b6d6e66190a593c511065c4fda524
HuyNX <huynxhe170420@fpt.edu.vn>
2024-10-30 21:48:05 +0700

    huynx branch local to remote repo

--- app/src/main/java/com/example/g2_movieapp/interfaces/TaskCallback.java
+++ app/src/main/java/com/example/g2_movieapp/interfaces/TaskCallback.java
@@ -2,7 +2,7 @@ package com.example.g2_movieapp.interfaces;
 
 
 public interface TaskCallback {
-    void onSuccess(String... params);
-    void onLoginSuccess();
-    void onFailure(String errorMess);
+    void onSuccess(String... params) ;
+    void onLoginSuccess() ;
+    void onFailure(String errorMess) ;
 }
